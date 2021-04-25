e9a7cf888f19c0f64f0f674edb231d06d1f27397
tazi nouhaila <66480477+tazi-pixel@users.noreply.github.com>
2021-04-25 22:50:19 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,55 +1,36 @@
 #include "blockchain.h"
 
-/**
-* create_genesis_block - creates genesis block
-* Return: a genesis block or NULL if failed
-*/
-block_t *create_genesis_block(void)
-{
-	block_t *genesis;
-
-	genesis = calloc(1, sizeof(*genesis));
-	if (!genesis)
-		return (NULL);
-
-	memcpy(genesis->data.buffer, GENESIS_DATA, GENESIS_DATA_LEN);
-	genesis->data.len = GENESIS_DATA_LEN;
-	*(genesis->data.buffer + GENESIS_DATA_LEN) = '\0';
-
-	memcpy(genesis->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
-	genesis->info.timestamp = GENESIS_TIMESTAMP;
-	return (genesis);
-}
+#define GENESIS_TIMESTAMP 1537578000
+#define GENESIS_DATA "Holberton School"
+#define GENESIS_DATA_LEN 16
+#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 
 /**
-* blockchain_create - creates a blockchain (Genesis Block)
-* Return: a new blockchain or NULL if failed
-*/
+ * blockchain_create - creates a genesis blockchain
+ * Return: pointer to new blockchain or NULL on error
+ */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *blockchain;
-	block_t *genesis;
+	blockchain_t *chain = calloc(1, sizeof(*chain));
+	block_t *block = calloc(1, sizeof(*block));
+	llist_t *list = llist_create(MT_SUPPORT_TRUE);
 
-	blockchain = malloc(sizeof(*blockchain));
-	if (!blockchain)
-		return (NULL);
-	genesis = create_genesis_block();
-	if (!genesis)
+	if (!chain || !block || !list)
 	{
-		free(blockchain);
+		free(chain), free(block), llist_destroy(list, 1, NULL);
 		return (NULL);
 	}
-	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
-	if (!blockchain->chain)
+
+	block->info.timestamp = GENESIS_TIMESTAMP;
+	memcpy(&(block->data.buffer), GENESIS_DATA, GENESIS_DATA_LEN);
+	block->data.len = GENESIS_DATA_LEN;
+	memcpy(&(block->hash), GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	if (llist_add_node(list, block, ADD_NODE_FRONT))
 	{
-		free(blockchain), free(genesis);
+		free(chain), free(block), llist_destroy(list, 1, NULL);
 		return (NULL);
 	}
-	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) == -1)
-	{
-		llist_destroy(blockchain->chain, 1, NULL);
-		free(blockchain), free(genesis);
-		return (NULL);
-	}
-	return (blockchain);
+	chain->chain = list;
+	return (chain);
 }
